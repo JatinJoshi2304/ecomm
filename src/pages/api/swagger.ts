28688863d39b1be3910c4bb25a43dec63a508f28
JatinJoshi2304 <jatin@thoughtwin.com>
2025-10-15 18:54:24 +0530

    Fix Swagger 404

--- src/pages/api/swagger.ts
+++ src/pages/api/swagger.ts
@@ -3,10 +3,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import YAML from "yaml";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (process.env.NODE_ENV === "production") {
-    res.status(404).end("Not found");
-    return;
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   res.status(404).end("Not found");
+  //   return;
+  // }
 
   const file = fs.readFileSync("src/swagger/swagger.yaml", "utf8");
   const swaggerDocument = YAML.parse(file);
